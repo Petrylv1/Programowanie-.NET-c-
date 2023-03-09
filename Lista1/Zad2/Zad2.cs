ba9ec70bd84e92624e53eb8c2b62ccc261948307
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-03-09 02:05:12 +0100

    Add files via upload

---
+++ Lista1/Zad2/Zad2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Zad2
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Wprowadź tekst: ");
+        string text = Console.ReadLine();
+
+        if (IsPalindrome(text))
+        {
+            Console.WriteLine("{0} jest palindromem", text);
+        }
+        else
+        {
+            Console.WriteLine("{0} nie jest palindromem", text);
+        }
+
+        Console.ReadKey();
+    }
+
+    static bool IsPalindrome(string text)
+    {
+        for (int i = 0; i < text.Length / 2; i++)
+        {
+            if (text[i] != text[text.Length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
