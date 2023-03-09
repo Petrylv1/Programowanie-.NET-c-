473f0bbd7dcff497d096430c0e60ceac685685a4
Petryl <62471248+Petrylv1@users.noreply.github.com>
2023-03-09 02:10:23 +0100

    Poprawka

--- Lista1/Zad3/Zad3.cs
+++ Lista1/Zad3/Zad3.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Zad3
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Wprowadź liczbę: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsTriangular(number))
+        {
+            Console.WriteLine("{0} jest liczbą trójkątną", number);
+        }
+        else
+        {
+            Console.WriteLine("{0} nie jest liczbą trójkątną", number);
+        }
+
+        Console.ReadKey();
+    }
+
+    static bool IsTriangular(int number)
+    {
+        int i = 1;
+        while (number > 0)
+        {
+            number -= i;
+            i++;
+        }
+
+        return number == 0;
+    }
+}
